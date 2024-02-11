bec07ca99bb7b82a432a7b66e760fc9225b16603
ZouCherif <cheriffzouaoui@gmail.com>
2024-02-11 23:15:56 +0100

    get request Done

--- client.c
+++ client.c
@@ -8,7 +8,98 @@
 #include <errno.h>
 #include <sys/time.h>
 
+#define SERVER_PORT 69
+#define MAX_PACKET_SIZE 516
+#define TIMEOUT_SECONDS 5
+#define MAX_RETRIES 3
 
+#define RRQ_OPCODE 1
+#define WRQ_OPCODE 2
+#define DATA_OPCODE 3
+#define ACK_OPCODE 4
+#define ERROR_OPCODE 5
+
+void handle_error_packet(const char *error_packet)
+{
+    int error_code = error_packet[3];
+    const char *error_message = error_packet + 4;
+
+    fprintf(stderr, "Error from server (Error code: %d): %s\n", error_code, error_message);
+}
+
+void handle_rrq(int client_socket, struct sockaddr_in server_addr, const char *filename)
+{
+    char rrq_packet[MAX_PACKET_SIZE];
+    rrq_packet[0] = 0; // Opcode RRQ
+    rrq_packet[1] = RRQ_OPCODE;
+    strcpy(rrq_packet + 2, filename);
+    rrq_packet[strlen(filename) + 2] = 0;
+
+    if (sendto(client_socket, rrq_packet, strlen(filename) + 3, 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error sending RRQ packet");
+        return;
+    }
+
+    struct timeval timeout;
+    timeout.tv_sec = TIMEOUT_SECONDS;
+    timeout.tv_usec = 0;
+    if (setsockopt(client_socket, SOL_SOCKET, SO_RCVTIMEO, (const char *)&timeout, sizeof(timeout)) < 0){
+        perror("Error setting timeout option");
+        return;
+    }
+
+    FILE *file = fopen(filename, "wb");
+    if (file == NULL){
+        char error_packet[MAX_PACKET_SIZE];
+        memset(error_packet, 0, MAX_PACKET_SIZE);
+        error_packet[0] = 0;
+        error_packet[1] = ERROR_OPCODE;
+        error_packet[2] = 0;
+        strcpy(error_packet + 4, "Failed to create file");
+
+        sendto(client_socket, error_packet, 4 + strlen("Failed to create file") + 1, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        return;
+    }
+
+    unsigned short block_number = 1;
+
+    while (1)
+    {
+        char data_packet[MAX_PACKET_SIZE];
+        ssize_t bytes_received = recvfrom(client_socket, data_packet, MAX_PACKET_SIZE, 0, NULL, NULL);
+        if (data_packet[1] == 5){
+            handle_error_packet(data_packet);
+            break;
+        }
+        
+        if (bytes_received < 0)
+        {
+            perror("Error receiving data packet");
+            break;
+        }
+        else if (bytes_received == 0){
+            fprintf(stderr, "Connection closed by server.\n");
+            break;
+        }
+
+        fwrite(data_packet + 4, 1, bytes_received - 4, file);
+
+        char ack_packet[4];
+        ack_packet[0] = 0;
+        ack_packet[1] = ACK_OPCODE;
+        ack_packet[2] = block_number >> 8;
+        ack_packet[3] = block_number & 0xFF;
+        sendto(client_socket, ack_packet, 4, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+        if (bytes_received < 512){   
+            break;
+        }
+        block_number++;
+    }
+
+    fclose(file);
+}
 
 int main(int argc, char *argv[])
 {
@@ -37,7 +128,7 @@ int main(int argc, char *argv[])
     server_addr.sin_port = htons(server_port);
 
     if (strcmp(operation, "put") == 0){
-        handle_wrq(server_socket, server_addr, filename);
+        // handle_wrq(server_socket, server_addr, filename);
     }
     else if (strcmp(operation, "get") == 0){
         handle_rrq(server_socket, server_addr, filename);
--- serveur/server.c
+++ serveur/server.c
@@ -9,6 +9,7 @@
 #include <sys/time.h>
 
 #define SERVER_PORT 69
+#define IP "127.0.0.1"
 #define MAX_PACKET_SIZE 516
 #define TIMEOUT_SECONDS 5
 
@@ -18,6 +19,108 @@
 #define ACK_OPCODE 4
 #define ERROR_OPCODE 5
 
+
+void send_error_packet(int server_socket, struct sockaddr_in client_addr, int error_code, const char *error_message)
+{
+    char error_packet[MAX_PACKET_SIZE];
+    memset(error_packet, 0, MAX_PACKET_SIZE);
+    error_packet[0] = 0; // Adding a null byte for the opcode
+    error_packet[1] = ERROR_OPCODE;
+    error_packet[2] = 0; // Adding a null byte for the error code
+    error_packet[3] = error_code;
+    strcpy(error_packet + 4, error_message);
+
+    sendto(server_socket, error_packet, strlen(error_message) + 5, 0, (struct sockaddr *)&client_addr, sizeof(client_addr));
+}
+
+void handle_request(int server_socket, struct sockaddr_in client_addr, char *filename, unsigned short opcode)
+{
+    switch (opcode)
+    {
+    case RRQ_OPCODE:
+        handle_rrq(server_socket, client_addr, filename);
+        break;
+    case WRQ_OPCODE:
+        // handle_wrq(server_socket, client_addr, filename);
+        break;
+    default:
+        printf("Unsupported opcode %d. Sending error packet to client\n", opcode);
+        send_error_packet(server_socket, client_addr, 1, "Unsupported operation");
+        break;
+    }
+}
+
+void handle_rrq(int server_socket, struct sockaddr_in client_addr, char *filename)
+{
+    printf("Handling Read Request (RRQ) from client\n");
+
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL)
+    {
+        send_error_packet(server_socket, client_addr, 1, "File not found");
+        perror("Error opening file for reading");
+        return;
+    }
+
+    unsigned short block_number = 1;
+    int attempts = 1;
+
+    while (1)
+    {
+        char data_packet[MAX_PACKET_SIZE];
+        ssize_t bytes_read = fread(data_packet + 4, 1, 512, file);
+
+        data_packet[1] = DATA_OPCODE;
+        data_packet[2] = block_number >> 8;
+        data_packet[3] = block_number & 0xFF;
+
+        if (sendto(server_socket, data_packet, 4 + bytes_read, 0, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0)
+        {
+            perror("Error sending data packet");
+            break;
+        }
+
+        char ack_packet[4];
+        ssize_t bytes_received = recvfrom(server_socket, ack_packet, 4, 0, NULL, NULL);
+        if (bytes_received < 0)
+        {
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+            {
+                if (attempts >= 2)
+                {
+                    fprintf(stderr, "Max retry attempts reached. Exiting...\n");
+                    break;
+                }
+                attempts++;
+                fprintf(stderr, "Timeout occurred, retrying...\n");
+                continue;
+            }
+            else
+            {
+                perror("Error receiving ACK packet");
+                break;
+            }
+        }
+        else if (bytes_received == 0)
+        {
+            fprintf(stderr, "Connection closed by client.\n");
+            break;
+        }
+
+        if (ack_packet[1] != ACK_OPCODE || (ack_packet[2] != (block_number >> 8)) || (ack_packet[3] != (block_number & 0xFF)))
+        {
+            fprintf(stderr, "Invalid ACK packet received. Exiting...\n");
+            break;
+        }
+
+        block_number++;
+
+        if (bytes_read < 512)
+            break;
+    }
+
+    fclose(file);
+}
 
 int main()
 {
@@ -34,7 +137,7 @@ int main()
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_addr.s_addr = inet_addr(IP);
     server_addr.sin_port = htons(SERVER_PORT);
 
     if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
